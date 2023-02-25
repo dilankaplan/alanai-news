d097793c15950f531b630110cdf6dcb3f876298c
Dilan Kaplan <45143995+dilankaplan@users.noreply.github.com>
2023-02-25 07:27:28 +0300

    material ui

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@mui/material'
 
-const NewsCard = () => {
+const NewsCard = ({article:{description, publishedAt, source, title, url , urlToImage} ,i}) => {
   return (
-    <div>
-        NewsCard
-    </div>
+    <Card>
+      <CardActionArea>
+        <CardMedia image="../images/3d-world-news-background-loop-free-video.jpg"  />
+        <div>
+          <Typography variant='body2' color="textSEcondary" component="h2"> {(new Date(publishedAt)).toDateString()} </Typography>
+          <Typography variant='body2' color="textSEcondary" component="h2"> {source.name} </Typography>
+        </div>
+         <Typography gutterBottom variant='h5'>{title} </Typography>
+         <CardContent>
+          <Typography variant='body2' color="textSecondary" component="p">{description}</Typography>
+         </CardContent>
+      </CardActionArea>
+
+      <CardActions>
+        <Button size='small' color='primary'>Learn More</Button>
+          <Typography variant='h5' color="textSecondary">{i + 1}</Typography>
+        
+      </CardActions>
+    </Card>
   )
 }
 
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import NewsCard from'../NewsCard/NewsCard'
-const NewsCards = ({articles}) => {
-  return (
-    <div>
-        {articles.map((articles, i)=>(
-            <NewsCard />
-        )
-    )}
-    </div>
-  )
-}
+import React from "react";
+import NewsCard from "../NewsCard/NewsCard";
+import { Grid, Grow, Typography } from "@mui/material";
 
-export default NewsCards
+import useStyles from'./styles'
+const NewsCards = ({ articles }) => {
+  const classes = useStyles();
+  
+  return (
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((articles, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+           
+            <NewsCard articles={articles} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  );
+};
+
+export default NewsCards;
