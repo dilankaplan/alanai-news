862f089cc5e71aa2edceac7b7134961727d0b731
Dilan Kaplan <45143995+dilankaplan@users.noreply.github.com>
2023-02-28 19:27:39 +0300

    cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {useState , useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import "./App.css";
 import NewsCards from "./components/NewsCards/NewsCards";
-//import Newscard from "./components/NewsCard/NewsCard";
+import classNames from "classnames";
 
+import useStyles from "./styles"
 const alanKey ='99ecbe618ae3f294c55d7bdce66fcc552e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
   const[newArticles , setNewsArticles] = useState([]);
+  const classes= useStyles();
   useEffect(()=>{
     alanBtn({
       key:alanKey,
@@ -19,7 +21,9 @@ const App = () => {
   },[])
   return (
     <div>
-      <h1>Hello app</h1>
+      <div className={classes.logoContainer}>
+      <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
       <NewsCards  articles={newArticles} />
      
     </div>
